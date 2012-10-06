7b06b26f506de241403a14f80b987dbe6c63cbc0
Jacob Cole <tmad4000@gmail.com>
2012-10-06 17:36:46 -0400

    morebackend

--- hackmodeonoff.php
+++ hackmodeonoff.php
@@ -15,12 +15,40 @@ print json_encode($rows);
 */
 	
 $sts = $_REQUEST['status']=="on" ? 1 : 0;
-$loginparams=array("'".$_REQUEST['email']."'",$_REQUEST['lat'],$_REQUEST['lng'],$sts,time());
+$loginparams=array("'".$_REQUEST['user_email']."'",$_REQUEST['lat'],$_REQUEST['lng'],$sts,time());
 
-$q='INSERT INTO users (email, latitude,longitude,status,status_update_time) VALUES ('.implode(',', $loginparams).')';
-echo $q."\n\n";
+if(!$_SESSION['user_email'])
+	die('Need $_SESSION[\'user_email\']');
+
+$q="SELECT id FROM users WHERE email='{$_SESSION['user_email']}'";
+//echo $q."\n\n";
 $result=mysql_query($q) or die("SELECT Error: ".mysql_error());
-
+$rows = array();
+while($r = mysql_fetch_assoc($result)) {
+	$rows[] = $r;
+}
+if(count($rows)<0) {
+	$q="UPDATE users (email, latitude,longitude,status,status_update_time) SET (".implode(',', $loginparams).") WHERE email='{$_SESSION['user_email']}'" ;
+	echo $q."\n\n";
+	$result=mysql_query($q) or die("SELECT Error: ".mysql_error());
+}
+else {
+	$q='INSERT INTO users (email, latitude,longitude,status,status_update_time) VALUES ('.implode(',', $loginparams).')';
+	//echo $q."\n\n";
+	$result=mysql_query($q) or die("SELECT Error: ".mysql_error());
+}
 
 $_SESSION['user_email']=$_REQUEST['user_email'];
+
+
+$q="SELECT * FROM users WHERE email <> '{$_SESSION['user_email']}'";
+//echo $q."\n\n";
+$result=mysql_query($q) or die("SELECT Error: ".mysql_error());
+
+$rows = array();
+while($r = mysql_fetch_assoc($result)) {
+	$rows[] = $r;
+}
+print json_encode($rows);
+
 ?>
